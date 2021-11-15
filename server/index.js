7ba2871dbbfd30d5f0d433d40057c5625c16ad11
davidkharbin <david_k_harbin@hotmail.com>
2021-11-15 09:55:09 -0600

    feat: confirmed login, data received from garmin-connect

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const queries = require('../database/schema.js');
 const db = require('../database/index.js')
 const cors = require('cors');
+const creds = require('../garmin.config.json')
  
 // serve static files from dist dir
 app.use(express.static('client/dist'));
@@ -29,3 +30,26 @@ const port = 3001;
 app.listen(port, () => {
  console.log(`Express server listening on port: ${port}`);
 });
+
+  
+const { GarminConnect } = require('garmin-connect');
+
+// Has to be run in an async function to be able to use the await keyword
+const main = async () => {
+    // Create a new Garmin Connect Client
+    const GCClient = new GarminConnect();
+
+    // Uses credentials from garmin.config.json
+    await GCClient.login(creds.username, creds.password);
+    const activities = await GCClient.getActivities(0, 1);
+
+    let pushUps = 0;
+    let pullUps = 0;
+    let sitUps = 0;
+    let burpees = 0;
+
+    console.log(activities[0].summarizedExerciseSets);
+};
+
+// Run the code
+main();
