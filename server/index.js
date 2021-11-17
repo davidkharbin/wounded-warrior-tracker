187a17b8b4e0f1d74b618af465e0d277f340dab0
davidkharbin <david_k_harbin@hotmail.com>
2021-11-16 20:40:08 -0600

    feat(garmin-connect): confirmed data response
    Successfully creation of Garmin Connect Client, received data, parsed
    for rep totals

--- server/index.js
+++ server/index.js
@@ -4,52 +4,78 @@ const queries = require('../database/schema.js');
 const db = require('../database/index.js')
 const cors = require('cors');
 const creds = require('../garmin.config.json')
- 
+const { GarminConnect } = require('garmin-connect');
+const port = 3001;
+
 // serve static files from dist dir
 app.use(express.static('client/dist'));
-
-// middleware
-// use express.json for parsing JSON
 app.use(express.json());
-// use cors middleware for enabling CORS with various options
 app.use(cors());
- 
 app.use(express.urlencoded({ extended: true }));
-// get index.html 
+
+app.listen(port, () => {
+  console.log(`Express server listening on port: ${port}`);
+});
+
+
+
+// Has to be run in an async function to be able to use the await keyword
+const main = async () => {
+  const totals = {
+    pushUps: 0,
+    pullUps: 0,
+    sitUps: 0,
+    burpees: 0
+  }
+  const workouts = [];
+
+  // Create a new Garmin Connect Client
+  const GCClient = new GarminConnect();
+  // Uses credentials from garmin.config.json
+  await GCClient.login(creds.username, creds.password);
+
+  //get last 30 activities
+  const activityList = await GCClient.getActivities(0, 30);
+  const lastActivity = await GCClient.getActivities(0, 1);
+
+  // aggregate relevant activities 
+  activityList.forEach(activity => {
+    if (activity.activityName.includes('Wounded')) {
+      const name = activity.activityName;
+      const date = activity.startTimeLocal.substring(0, 10)
+      const id = activity.activityId;
+      const summary = activity.summarizedExerciseSets;
+      workouts.push({ name: name, id: id, summary: summary, date: date })
+    }
+  });
+
+  workouts.forEach(workout => {
+    const summaries = workout.summary;
+    summaries.forEach(summary => {
+      if (summary.category === 'PULL_UP') totals.pullUps += summary.reps;
+      if (summary.category === 'PUSH_UP') totals.pushUps += summary.reps;
+      if (summary.category === 'SIT_UP') totals.sitUps += summary.reps;
+      if (summary.subCategory === 'BURPEE') totals.burpees += summary.reps;
+    })
+  })
+  console.log('=========TOTAL TO DATE========');
+  console.log('pushUps :>> ', totals.pushUps);
+  console.log('pullUps :>> ', totals.pullUps);
+  console.log('sitUps :>> ', totals.sitUps);
+  console.log('burpees :>> ', totals.burpees);
+  return totals;
+};
+
+app.get('/totals', (req, res) => {
+  console.log('ran totals');
+  res.send(main())
+})
+
 app.get('/', (req, res) => {
+  console.log('vistited/');
   res.sendFile('index.html', { root: __dirname + '/../client/dist' }, (err) => {
     if (err) {
       res.status(400).send(err);
     }
   });
 });
-// set port where server will listen
-const port = 3001;
- 
-// tell server to listen on predefined port
-app.listen(port, () => {
- console.log(`Express server listening on port: ${port}`);
-});
-
-  
-const { GarminConnect } = require('garmin-connect');
-
-// Has to be run in an async function to be able to use the await keyword
-const main = async () => {
-    // Create a new Garmin Connect Client
-    const GCClient = new GarminConnect();
-
-    // Uses credentials from garmin.config.json
-    await GCClient.login(creds.username, creds.password);
-    const activities = await GCClient.getActivities(0, 1);
-
-    let pushUps = 0;
-    let pullUps = 0;
-    let sitUps = 0;
-    let burpees = 0;
-
-    console.log(activities[0].summarizedExerciseSets);
-};
-
-// Run the code
-main();
