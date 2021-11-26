cd3fdf49c0627ee8a4476eb2f7d270b6fd617939
davidkharbin <david_k_harbin@hotmail.com>
2021-11-25 20:19:23 -0700

    refactor: renamed offline.js

--- server/index.js
+++ server/index.js
@@ -1,19 +1,19 @@
+// const queries = require('../database/schema.js');
+// const db = require('../database/index.js')
 const express = require('express');
 const app = express();
 const cron = require('node-cron');
-// const queries = require('../database/schema.js');
-// const db = require('../database/index.js')
 const cors = require('cors');
 const creds = require('../garmin.config.json')
 const { GarminConnect } = require('garmin-connect');
 const port = process.env.PORT || 3000;
 
+// express server w/cors
 // serve static files from dist dir
 app.use(express.static('client/dist'));
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-
 app.listen(port, () => {
   console.log(`Express server listening on port: ${port}`);
 });
@@ -28,27 +28,26 @@ const main = () => {
   let workouts = [];
 
   (async function getData() {
-    // Create a new Garmin Connect Client
+    // Create a new Garmin Connect Client and login
     let GCClient = new GarminConnect();
-    // Uses credentials from garmin.config.json
     await GCClient.login(creds.username, creds.password);
-    //get last 50 activities
-    let activityList = await GCClient.getActivities(0, 50);
-    // filter Wounded-Warrior activities 
 
-          // === === === TO DO === === === //
-    // **** refactor using arr.proto.filter ****
-    activityList.forEach(activity => {
-      if (activity.activityName.includes('Wounded')) {
+    // get last 50 activities
+    let activityList = await GCClient.getActivities(0, 50);
+
+    // get the activities named Wounded-Warrior
+    let activities = activityList.filter(activity => activity.activityName.includes('Wounded'));
+    
+    // get desired data from each activity
+    activities.forEach(activity => {
         let name = activity.activityName;
         let date = activity.startTimeLocal.substring(0, 10)
         let id = activity.activityId;
         let summary = activity.summarizedExerciseSets;
         workouts.push({ name: name, id: id, summary: summary, date: date })
-      }
     });
 
-    // total the remaining reps for each exercise
+    // get total reps of each exercise, for each workout
     workouts.forEach(workout => {
       let summaries = workout.summary;
       summaries.forEach(summary => {
@@ -64,16 +63,15 @@ const main = () => {
     console.log('sitUps  :>>  ', totals.sitUps);
     console.log('burpees :>> ', totals.burpees);
   })();
-  // send back the total reps of each exercise 
-  // send back the list of workouts
-  // send back 
+  
+  // total reps, list of workout data
   return [totals, workouts];
 };
 
 let garminData = main();
 
-// refresh data every 6 hours
-cron.schedule('0 */6 * * *', () => {
+// refresh data every 4 hours
+cron.schedule('0 */4 * * *', () => {
   garminData = main();
 });
 
