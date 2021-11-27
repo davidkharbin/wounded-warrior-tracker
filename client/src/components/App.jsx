f37d148fb47d340dc356fbbd59f7f5217af82059
davidkharbin <david_k_harbin@hotmail.com>
2021-11-27 10:36:40 -0700

    refactor(App.jsx): separate sorting function

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,6 +7,10 @@ import Exercise from './Burpees.jsx';
 const App = () => {
 	const [totals, setTotals] = useState('');
 	const [workouts, setWorkouts] = useState([]);
+	
+	const sortWorkoutsAsc = (firstWorkout, nextWorkout) => {
+		return firstWorkout.id - nextWorkout.id;
+	};
 
 	const fetchTotals = () => {
 		axios.get(`/totals`, {
@@ -14,9 +18,7 @@ const App = () => {
 			.then((response) => {
 				setTotals(response.data[0]);
 				setWorkouts(response.data[1]
-					// === === === TO DO === === === //
-					// split the sort function out
-					.sort((firstWorkout, nextWorkout) => firstWorkout.id - nextWorkout.id));
+					.sort(sortWorkoutsAsc));
 			});
 	};
 
@@ -29,13 +31,13 @@ const App = () => {
 			<h1>Wounded Warrior Challenge 2021</h1>
 			<h3>Goal:</h3>
 			<p> <span className="strikethrough">830 burpees in November</span></p>
-					<p>1000 burpees + 1000 sit-ups + 1000 push-ups + 1000 pull-ups in November!</p>
+			<p>1000 burpees + 1000 sit-ups + 1000 push-ups + 1000 pull-ups in November!</p>
 			<h3>Totals To Date: </h3>
 			<ul className="App-totals">
-				<Exercise name="Burpees"  total={totals.burpees} />
+				<Exercise name="Burpees" total={totals.burpees} />
 				<Exercise name="Pull-Ups" total={totals.pullUps} />
 				<Exercise name="Push-Ups" total={totals.pushUps} />
-				<Exercise name="Sit-Ups"  total={totals.sitUps}  />
+				<Exercise name="Sit-Ups" total={totals.sitUps} />
 			</ul>
 			<h3>Records: </h3>
 			<ul>
