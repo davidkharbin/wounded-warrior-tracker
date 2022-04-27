ce29ee48fe0e212bbccba9977e75863a2be07c5e
davidkharbin <david_k_harbin@hotmail.com>
2022-04-27 10:46:17 -0600

    build: swap Roboto font package for CDN link

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,9 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 
 ReactDOM.render(<App />, document.getElementById('app'));
